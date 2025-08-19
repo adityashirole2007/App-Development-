41370e39a2bada6158d622f25ad6f7b255296703
adityashirole2007 <adityashirole30@gmail.com>
2025-08-20 00:12:29 +0530

    integerType

---
+++ IntegerType/src/main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var integerMaxValue = Int.MAX_VALUE
+    var integerMinValue = Int.MIN_VALUE
+    println("Integer Max Value: $integerMaxValue")
+    println("Integer Min Value: $integerMinValue")
+
+    val byteMaxValue: Byte = Byte.MAX_VALUE
+    println("Byte Max Value: $byteMaxValue")
+//    ans
+//    Integer Max Value: 2147483647
+//    Integer Min Value: -2147483648
+//    Byte Max Value: 127
+//    short long kar lena ab
+}

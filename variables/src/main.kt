c081fdd465302f70f60d201fc4d289590b6dba4c
adityashirole2007 <adityashirole30@gmail.com>
2025-08-20 00:03:53 +0530

    Variables

---
+++ variables/src/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val userName : String = "Aaditya"
+    // val ki value bad mein change nahi hoti
+    println(userName)
+    var age : Int = 18
+    println("Hello $userName your age is $age")
+}
